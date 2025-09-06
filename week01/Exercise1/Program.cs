1593c37840264760ec021f53ab86c0e93e7566fb
ecindy <ecindycruz@gmail.com>
2025-09-06 16:42:42 -0600

    Finished Prep-1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        //Prompt the user for their first name
+        Console.Write("What is your first name? ");
+        string First_name = Console.ReadLine();
+
+        //prompt them for their last name.
+        Console.Write("What is your last name? ");
+        string Last_name = Console.ReadLine();
+        
+        //Display the text back all on one line saying, "Your name is last-name, first-name, last-name"
+        Console.WriteLine($"Your name is {Last_name}, {First_name} {Last_name}");
+
     }
 }
