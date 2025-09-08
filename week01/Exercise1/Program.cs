4e8a7cbc62305e4e93957f700a4b7868b17755cd
ecindy <ecindycruz@gmail.com>
2025-09-07 18:23:25 -0600

    Finished exercise2

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,13 +5,16 @@ class Program
     static void Main(string[] args)
 
     {
+        //Prompt the user for their first name.
         Console.WriteLine("What is your first name? ");
         string first = Console.ReadLine();
 
+        //Prompt the user for their last name.
         Console.WriteLine("What is your last name? ");
         string last = Console.ReadLine();
 
+        //"Display the text back all on one line saying, "Your name is last-name, first-name, last-name".
         Console.WriteLine($"Your name is {last}, {first} {last}");
     }
 
-}        //Prompt the user for their first 
+}        
